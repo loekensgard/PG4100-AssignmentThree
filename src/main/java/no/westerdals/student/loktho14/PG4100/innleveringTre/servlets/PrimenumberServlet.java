5ad9284a17b1eff5dd26f644149fcb56adf4200a
Thorstein <loktho14@student.westerdals.no>
2016-04-15 20:12:52 +0200

    Fixed somethings

--- src/main/java/no/westerdals/student/loktho14/PG4100/innleveringTre/servlets/PrimenumberServlet.java
+++ src/main/java/no/westerdals/student/loktho14/PG4100/innleveringTre/servlets/PrimenumberServlet.java
@@ -1,3 +1,7 @@
+package no.westerdals.student.loktho14.PG4100.innleveringTre.servlets;
+
+import no.westerdals.student.loktho14.PG4100.innleveringTre.utils.NumberChecker;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -5,6 +9,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Created by Thorstein on 14.04.2016.
@@ -12,6 +20,7 @@ import java.io.PrintWriter;
 @WebServlet(name ="primeNumberServlet", urlPatterns = "/primeNumberServlet")
 public class PrimenumberServlet extends HttpServlet{
     private NumberChecker nc;
+    private static final Logger logger = LogManager.getLogger("Requests");
 
 
     @Override
@@ -21,16 +30,27 @@ public class PrimenumberServlet extends HttpServlet{
 
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.info("Debug : Requesting parameter" + " - Date: " + new Date().toString());
         String primeNumber = req.getParameter("primeNumberServlet");
+        logger.info("Debug : Users number is " + primeNumber + " - Date: " + new Date().toString());
         nc = new NumberChecker(primeNumber);
+        resp.setContentType("text/html");
 
+        output(resp);
+
+    }
+
+    private void output(HttpServletResponse resp) throws IOException {
         try (PrintWriter out = resp.getWriter()) {
+            logger.info("Debug : Output started" + " - Date: " + new Date().toString());
             int number = nc.getNumber();
-
+            out.print("<center>");
+            out.print("<h1>");
             out.println(nc.toString(number));
-
+            out.print("</h1>");
+            out.print("</center>");
+            logger.info("Debug : Output stopped"+ " - Date: " + new Date().toString());
         }
-
     }
 
 }
---
+++ src/main/java/no/westerdals/student/loktho14/PG4100/innleveringTre/utils/NumberChecker.java
@@ -0,0 +1,48 @@
+package no.westerdals.student.loktho14.PG4100.innleveringTre.utils; /**
+ * Created by Thorstein on 14.04.2016.
+ */
+
+import org.apache.commons.math3.primes.Primes;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.Arrays;
+
+public class NumberChecker {
+    private int number = 0;
+    private static final Logger logger = LogManager.getLogger("Errors");
+
+    public NumberChecker(String nr){
+        if(nr.startsWith("-")){
+            logger.error("Error : User tried to send in negative number");
+            throw new ArithmeticException("Negative numbers can not be primes");
+        }else{
+            isNumber(nr);
+        }
+    }
+
+    private void isNumber(String nr) {
+        try{
+            number = Integer.parseInt(nr);
+        }catch (NumberFormatException e){
+            logger.error("Error : Couldn't parse string to integer" + Arrays.toString(e.getStackTrace()));
+            throw new NumberFormatException("Couldn't parse string to integer");
+        }
+    }
+
+    public int getNumber(){
+        return number;
+    }
+
+    public boolean isPrime(int number){
+        return Primes.isPrime(number);
+    }
+
+    public String toString(int number){
+        if((!isPrime(number))){
+            return number + " er ikke et primtall";
+        }else{
+            return number + " er et primtall";
+        }
+    }
+
+}
